9e2197a58e2261154f598e69aca92422230a8d0f
Emmanuel Marcha <info@emmanuelmarcha.com>
2014-06-03 14:13:22 -0500

    Update logos and layout

--- app/js/main.js
+++ app/js/main.js
@@ -12,16 +12,22 @@ app.controller('HomeCtrl', ['$scope', function($scope) {
 
   $scope.events = [
     {
-      name: 'Some Event Name',
-      description: 'Some description'
+      name: 'Re-Imagination Challenge',
+      date: '3/29/14 - 3/30/14',
+      description: 'Put the money where your mouth is and create some innovative ideas to change the face of IIT. Get a team together to improve our school in this Hackathon style challenge. ',
+      logo: '/img/events/reimagination.png'
     },
     {
-      name: 'Some Other Event Name',
-      description: 'Some other description'
+      name: 'Gogo Hackathon',
+      date: '2/22/14 - 2/23/14',
+      description: 'Generate a product or idea that helps air travelers stay connected to the things they care about most - loved ones, entertainment, or work.',
+      logo: '/img/events/gogo.png'
     },
     {
-      name: 'Some New Event',
-      description: 'Some other new description'
+      name: 'The Forge',
+      date: '10/26/13 - 10/27/13',
+      description: 'Celebrate Chicago’s emergence as a global innovation hub by taking part in the action at the Idea Shop.',
+      logo: '/img/events/forge.png'
     }
   ];
 
