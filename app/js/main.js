b522b97051b993f3bd7b1aefda1e22462c5e3539
Emmanuel Marcha <info@emmanuelmarcha.com>
2014-06-04 00:52:03 -0500

    Update event wording and layout

--- app/js/main.js
+++ app/js/main.js
@@ -14,19 +14,19 @@ app.controller('HomeCtrl', ['$scope', function($scope) {
     {
       name: 'Re-Imagination Challenge',
       date: '3/29/14 - 3/30/14',
-      description: 'Put the money where your mouth is and create some innovative ideas to change the face of IIT. Get a team together to improve our school in this Hackathon style challenge. ',
+      description: 'Get a team together to improve IIT.',
       logo: '/img/events/reimagination.png'
     },
     {
       name: 'Gogo Hackathon',
       date: '2/22/14 - 2/23/14',
-      description: 'Generate a product or idea that helps air travelers stay connected to the things they care about most - loved ones, entertainment, or work.',
+      description: 'Generate a product or idea that helps air travelers stay connected.',
       logo: '/img/events/gogo.png'
     },
     {
       name: 'The Forge',
       date: '10/26/13 - 10/27/13',
-      description: 'Celebrate Chicago’s emergence as a global innovation hub by taking part in the action at the Idea Shop.',
+      description: 'Celebrate Chicago’s emergence as a global innovation hub.',
       logo: '/img/events/forge.png'
     }
   ];
